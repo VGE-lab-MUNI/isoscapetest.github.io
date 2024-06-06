6cf1d6e3aac1db2926bcaf612b78ddcbe2e4cc2b
Ondřej Kvarda <kvarda.ondrej@mail.muni.cz>
2024-06-06 15:45:21 +0200

    4

--- script.js
+++ script.js
@@ -1,137 +1,113 @@
-(function () {
-    "use strict";
-}());
+var map = L.map('map').setView([39.449811170044626, -8.206305119865776], 6);
 
-var map = L.map('map').setView([48.95, 16.59], 9);
 
 var CartoDB_PositronNoLabels = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png', {
-	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-	subdomains: 'abcd',
-	maxZoom: 20,
-    minZoom: 9
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    subdomains: 'abcd',
+    maxZoom: 20,
+    minZoom: 1
 }).addTo(map);
 
-L.control.defaultExtent()
-  .addTo(map);
 
-const settlementsIconPresent = L.icon({
-    iconUrl: "markerPresent.svg",
-    iconSize: [10, 10]
-});
+const hexagrid = "https://gist.githubusercontent.com/VGE-lab-MUNI/b363c6dbb7fd390680c10287ba67ff4f/raw/0d96ba2c7ba5873c7f8385d36d96abfd84dbc2bd/PortugalIsoHexa.geojson"
+var hexa = new L.GeoJSON.AJAX(hexagrid, {style: gridColor, onEachFeature: displayValue});
+map.addLayer(hexa);
 
-const settlementsIconNotPresent = L.icon({
-    iconUrl: "markerNotPresent.svg",
-    iconSize: [10, 10]
-});
 
-const settlements = "https://gist.githubusercontent.com/VGE-lab-MUNI/54c20ac7bfad5f260631e39f49bd0b74/raw/2998a9a831db6f048ade20a2f304177981c807f9/hutter_points.geojson";
 
-const dwellings = "https://gist.githubusercontent.com/VGE-lab-MUNI/6a04baa6f4b6ad6525c208fdcacd608e/raw/a9187314740772563fc88e05b0387e0a4b2dab10/hutter.geojson"
-    
-var hutteritesDwellings = new L.GeoJSON.AJAX(dwellings, {style: dwellingsStyle, onEachFeature: nameDisplayDwellings});
+var imageUrl = 'https://i.ibb.co/ZK39vXm/Layout.png',
+    imageBounds = [[36.962295943144255, -9.509959545772267], [42.14680090712174, -6.1849077721580885]],
+    image = L.imageOverlay(imageUrl, imageBounds).addTo(map);
 
-var hutteritesSettlements = new L.GeoJSON.AJAX(settlements, {pointToLayer: markerDisplay, onEachFeature: nameDisplaySettlements}).addTo(map);
-
-function dwellingsStyle(feature) {
-    if (feature.properties.Certainty == 1) {
-        return {
-    fillColor: "#AA9EFF",
-    color: "#0000dc",
-    weight: 2,
-    fillOpacity: 0.7
-  } 
-    } else if (feature.properties.Certainty == 2) {
-        return {
-    fillColor: "#c8c0ff",
-    color: "#0000dc",
-    dashArray: "5, 5",
-    weight: 2,
-    fillOpacity: 0.7
-  }
-    } else {
-        return {
-    fillColor: "#e3dfff",
-    color: "#0000dc",
-    dashArray: "0.1, 5",
-    weight: 2,
-    fillOpacity: 0.7
-  }
+map.on('zoomend', function(){
+    if (map.getZoom() > 8) 
+        {
+            image.setStyle({opacity: 0.001});
+        } else {
+            image.setStyle({opacity: 1});
+        }
     }
-    
-};
+);
 
-hutteritesSettlements.on('click', function(e) {
-    map.setView(e.latlng, 15);
-});
-
-function markerDisplay(feature, latlng) {
-    
-    if (feature.properties.Dwellings == "N") 
-    
-    {
-    
-        return L.marker(latlng, {icon:settlementsIconNotPresent}); 
-    }
-    
-    else 
-    
-    {
-        
-        return L.marker(latlng, {icon:settlementsIconPresent});
-    }
-}
-
-function nameDisplaySettlements(feature, layer) {
-    if (feature.properties.Dwellings == "N") {
-        label = "<center>" + "<b>" + String(feature.properties.Settlement);
-    } else {
-        label = "<center>" + "<b>" + String(feature.properties.Settlement) + "<br>" + "</b>" + "Počet identifikovaných lokací: " + String(feature.properties.Count);
-    }
-    layer.bindTooltip(label, {className: "my-labels", direction: "top"});
-};
-
-function nameDisplayDwellings(feature, layer) {
+function displayValue(feature, layer) {
    
-    label = "<center>" + "<b>" + String(feature.properties.Dwelling) + "<br>" + "Jistota lokalizace: " + "</b>" + String(feature.properties.Certainty);
+    label = "<center>" + "87Sr/86Sr mean value: " + "<b>" + String(Math.round(feature.properties.MEAN* 10000) / 10000);
     
     layer.bindTooltip(label, {className: "my-labels", direction: "top"});
+
+
 };
 
 
-
-map.on('zoomend', function(){
-    var element = document.getElementById("hide-dwellings");
-    if (map.getZoom() > 13) 
-        
-        {
-            map.addLayer(hutteritesDwellings);
-            element.style.display = "block";
-        } 
-    
-        else
-            
-        {
-            map.removeLayer(hutteritesDwellings);
-            element.style.display = "none";
+function gridColor(feature) {
+    if (feature.properties.MEAN < 0.710899) {
+        return {
+            fillColor: "#5E4FA2",
+            color: "#5E4FA2",
+            weight: 0,
+            fillOpacity: 0.5,
         }
-    }
-);
-
-
-map.on('zoomend', function(){
-    var element = document.getElementById("hide-settlements");
-    if (map.getZoom() > 13) 
-        
-        {
-            map.removeLayer(hutteritesSettlements);
-            element.style.display = "none";
-        } 
-    
-        else
-            
-        {
-            map.addLayer(hutteritesSettlements);
-            element.style.display = "block";
+    } else if (feature.properties.MEAN < 0.712581) {
+        return {
+            fillColor: "#388EBA",
+            color: "#388EBA",
+            weight: 0,
+            fillOpacity: 0.5
         }
-    }
-);
+    } else if (feature.properties.MEAN < 0.714362) {
+        return {
+            fillColor: "#75C8A5",
+            color: "#75C8A5",
+            weight: 0,
+            fillOpacity: 0.5
+        } 
+    } else if (feature.properties.MEAN < 0.71646) {
+        return {
+            fillColor: "#BFE5A0",
+            color: "#BFE5A0",
+            weight: 0,
+            fillOpacity: 0.5
+        } 
+    } else if (feature.properties.MEAN < 0.718723) {
+        return {
+            fillColor: "#F1F9A9",
+            color: "#F1F9A9",
+            weight: 0,
+            fillOpacity: 0.5
+        }
+    } else if (feature.properties.MEAN < 0.720876) {
+        return {
+            fillColor: "#FEEEA2",
+            color: "#FEEEA2",
+            weight: 0,
+            fillOpacity: 0.5
+        }
+    } else if (feature.properties.MEAN < 0.723293) {
+        return {
+            fillColor: "#FDBF6F",
+            color: "#FDBF6F",
+            weight: 0,
+            fillOpacity: 0.5
+        }
+    } else if (feature.properties.MEAN < 0.725902) {
+        return {
+            fillColor: "#F67B4A",
+            color: "#F67B4A",
+            weight: 0,
+            fillOpacity: 0.5
+        }
+    } else if (feature.properties.MEAN < 0.728924) {
+        return {
+            fillColor: "#D8434E",
+            color: "#D8434E",
+            weight: 0,
+            fillOpacity: 0.5
+        }
+    } else {
+        return {
+            color: "#9E0142",
+            weight: 0,
+            fillOpacity: 0.5
+        }
+}    
+}
