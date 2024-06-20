bd0a7eb02ac25117fba1541d6ddd15d2d2b3b914
Ondřej Kvarda <451448@mail.muni.cz>
2024-06-21 01:13:06 +0200

    18

--- mapScript.js
+++ mapScript.js
@@ -1,8 +1,69 @@
-var map = L.map('map').setView([39.449811170044626, -8.206305119865776], 6);
+var map = L.map('map', {minZoom:6, maxZoom:20}).setView([39.449811170044626, -8.206305119865776], 6);
 
-var lastClicked;
-var isFiltering = false;
-const { GLOperations } = L.TileLayer;
+var CartoDB_PositronNoLabels = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    subdomains: 'abcd',
+    maxZoom: 20,
+    minZoom: 6
+}).addTo(map);
+
+const boundaries = "https://gist.githubusercontent.com/VGE-lab-MUNI/1fdc942f1d359ad4968b8409dc8ce801/raw/9c85a7d415e7ad27cb0f2de1ca8ed4b121486897/portugalBound.geojson";
+
+fetch("../GeoTIFF/EBK.tif")
+  .then(response => response.arrayBuffer())
+  .then(arrayBuffer => {
+    parseGeoraster(arrayBuffer).then(georaster => {
+
+      var layer = new GeoRasterLayer({
+          georaster: georaster,
+          opacity: 0.7,
+          pixelValuesToColorFn: function (value) {
+            if (value > 0.7063 && value < 0.7082) {
+                return "#3DA1D1";
+            } else if (value > 0.7082 && value < 0.7096) {
+                return "#75B3C3";
+            } else if (value > 0.7096 && value < 0.7106) {
+                return "#B9D6A2";
+            } else if (value > 0.7106 && value < 0.7112) {
+                return "#9AC5B4";
+            } else if (value > 0.7112 && value < 0.7122) {
+                return "#D7EA90";
+            } else if (value > 0.7122 && value < 0.7136) {
+                return "#F1FB7B";
+            } else if (value > 0.7136 && value < 0.7155) {
+                return "#F6D865";
+            } else if (value > 0.7155 && value < 0.7182) {
+                return "#F9B552";
+            } else if (value > 0.7182 && value < 0.7220) {
+                return "#F88F3E";
+            } else if (value > 0.7220 && value < 0.7273) {
+                return "#F5652C";
+            } else if (value > 0.7273 && value < 0.7349) {
+                return "#F0261C";
+            } else {
+                return "transparent";
+            }
+          },
+          resolution: 512
+      });
+      layer.addTo(map);
+
+      //map.fitBounds(layer.getBounds());
+
+      map.on("click", function(event) {
+        var lat = event.latlng.lat;
+        var lng = event.latlng.lng;
+        var value = geoblaze.identify(georaster, [lng, lat]);
+
+        layer.bindPopup("<center>" + "<sup>" + "87" + "</sup>" + "Sr/" + "<sup>" + "86" + "</sup>" + "Sr" + " mean value: " + "<b>" + String(Math.round(value* 10000) / 10000)).openPopup([lat, lng]);
+      });
+  });
+});
+
+
+
+//var lastClicked;
+//var isFiltering = false;
 
 L.control.defaultExtent()
   .addTo(map);
@@ -19,18 +80,10 @@ var printPlugin = L.easyPrint({
 }).addTo(map);
 
 
-
 map.attributionControl.addAttribution('Created by: <b>Ondrej Kvarda</b>');
 
 
-var CartoDB_PositronNoLabels = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-    subdomains: 'abcd',
-    maxZoom: 20,
-    minZoom: 6
-}).addTo(map);
-
-
+/* 
 const hexagrid = "https://gist.githubusercontent.com/VGE-lab-MUNI/b363c6dbb7fd390680c10287ba67ff4f/raw/0d96ba2c7ba5873c7f8385d36d96abfd84dbc2bd/PortugalIsoHexa.geojson"
 var hexa = new L.GeoJSON.AJAX(hexagrid, {style: gridColor, onEachFeature: displayValue});
 var hexa2 = new L.GeoJSON.AJAX(hexagrid, {style: gridFilter, onEachFeature: displayValue});
@@ -38,9 +91,8 @@ var hexa2 = new L.GeoJSON.AJAX(hexagrid, {style: gridFilter, onEachFeature: disp
 
 
 var imageUrl = 'https://i.ibb.co/hcqBH3c/Port.png',
-    imageBounds = [[36.962295943144255, -9.509959545772267], [42.14680090712174, -6.1849077721580885]],
-    image = L.imageOverlay(imageUrl, imageBounds).addTo(map);
-
+imageBounds = [[36.962295943144255, -9.509959545772267], [42.14680090712174, -6.1849077721580885]],
+image = L.imageOverlay(imageUrl, imageBounds).addTo(map);
 
 
 
@@ -57,8 +109,6 @@ map.on('zoomend', function(){
         }
     }
 );
-
-
 
 
 
@@ -243,11 +293,4 @@ var customControl =  L.Control.extend({
 
  map.addControl(new customControl());
 
-
-
-
-
-
-
-
-
+ */
